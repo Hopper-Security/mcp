f4a5f90c39f4b98f0c6935c4a9303fd6eb9c4e24
Noy Duany <154744620+noy-duany@users.noreply.github.com>
2025-06-25 12:29:30 +0300

    MCP server v1.1 - Add imports to server and change mcp server name (#2)
    * mcp server v1.1

--- server.py
+++ server.py
@@ -1,4 +1,10 @@
 from src.mcp_instance import mcp
 
+# pylint: disable=unused-import
+import src.licenses
+import src.vulnerabilities
+import src.ai
+# pylint: enable=unused-import
+
 if __name__ == "__main__":
     mcp.run()
